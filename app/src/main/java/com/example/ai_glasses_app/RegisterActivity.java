e245659ec0a0396b92273f3f29439edc4fa22f40
Abilaash <abilaash2001@gmail.com>
2023-11-22 18:36:08 -0500

    authentication verification and navigation between pages

--- app/src/main/java/com/example/ai_glasses_app/RegisterActivity.java
+++ app/src/main/java/com/example/ai_glasses_app/RegisterActivity.java
@@ -3,6 +3,7 @@ package com.example.ai_glasses_app;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
@@ -13,9 +14,6 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.android.material.textfield.TextInputEditText;
-import com.google.android.material.textfield.TextInputLayout;
-import com.google.firebase.Firebase;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -27,6 +25,18 @@ public class RegisterActivity extends AppCompatActivity {
     FirebaseAuth mAuth;
 
     @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
@@ -34,6 +44,7 @@ public class RegisterActivity extends AppCompatActivity {
         editEmail = findViewById(R.id.email);
         editUser = findViewById(R.id.username);
         editPass = findViewById(R.id.password);
+        btnReg = findViewById(R.id.createbtn);
 
         btnReg.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -63,10 +74,14 @@ public class RegisterActivity extends AppCompatActivity {
                             @Override
                             public void onComplete(@NonNull Task<AuthResult> task) {
                                 if (task.isSuccessful()) {
-//                                    // Sign in success, update UI with the signed-in user's information
-//                                    FirebaseUser user = mAuth.getCurrentUser();
+                                    // Sign in success, update UI with the signed-in user's information
+                                    // FirebaseUser user = mAuth.getCurrentUser();
                                     Toast.makeText(RegisterActivity.this, "Account Created.",
                                             Toast.LENGTH_SHORT).show();
+
+                                    Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                                    startActivity(intent);
+                                    finish();
                                 } else {
                                     // If sign in fails, display a message to the user.
                                     Toast.makeText(RegisterActivity.this, "Authentication failed.",
